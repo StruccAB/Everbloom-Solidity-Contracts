d866e55c5c714571a95dccf0839d9baca577d914
shehryarshoukat <14bscssshoukat@seecs.edu.pk>
2023-02-09 04:04:29 +0500

    update upgradable-deploy-matic.ts

--- scripts/upgradable-deploy-matic.ts
+++ scripts/upgradable-deploy-matic.ts
@@ -3,7 +3,6 @@ import { ethers, upgrades } from "hardhat";
 async function main() {
   const EverDropManager = await ethers.getContractFactory("EverDropManager");
   const EverNFT = await ethers.getContractFactory("EverNFT");
-  const UsdcAddress = '0x0fa8781a83e46826621b3bc094ea2a0212e71b23'
 
   const everDropManager = await upgrades.deployProxy(EverDropManager, [process.env.PUBLIC_KEY], { kind: 'uups' })
   await everDropManager.deployed();
@@ -12,9 +11,8 @@ async function main() {
       EverNFT,
       [
           everDropManager.address,
-          UsdcAddress,
           process.env.PUBLIC_KEY,
-          'https://api-dev.everbloom.app/v1/ever-nft/',
+          'https://api-dev.everbloom.app/v1/metadata/',
           'EverNFT',
           'EverNFT',
       ],
@@ -34,7 +32,7 @@ main().catch((error) => {
 });
 
 /*
-EverDropManager Contract deployed to address: 0xd8FCB53965E3646EA11B0972eBb5f17eC4f36021
-EverNFT Contract deployed to address: 0x9cbf8826C78eAE0ae9BCf8C1765ad8330ff78ceF
+EverDropManager Contract deployed to address: 0x1C01C375Fb86BF6f1959F74cbEb4834e4277ec1d
+EverNFT Contract deployed to address: 0x02C0852b92ef8e23FA73873f4b99D5fC8D1aAD85
 */
 
